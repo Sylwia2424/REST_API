00b2d056705f855c1f5c4c4ad677681aa03e977a
Sylwia <sylwia_24_24@wp.pl>
2020-03-12 16:34:35 +0100

    Add route, folder

---
+++ routes/concerts.routes.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const db = require('../db.js');
+const router = express.Router();
+
+router.route('/concerts').get((req, res) => {
+  res.json(db.concerts);
+});
+
+router.route('/concerts/:id').get((req, res) => {
+  let newDb = db.concerts((item) => { 
+    return item.id == req.params.id;
+  })
+  res.json(newDb);
+});
+
+
+router.route('/concerts/').post((req, res) => {
+  const { performer, genre, price, day, image } = req.body;
+  db.push({id: (db[db.length -1].id +1),
+    performer, genre, price, day, image});
+  res.send({ message: 'OK' }); 
+});
+
+router.route('/concerts/:id').put((req, res) => {
+  const { performer, genre, price, day, image } = req.body;
+  const { id } = req.params;
+
+  db.map((item) => {
+    // console.log(item);
+    if(item.id == id){
+      item.performer = performer;
+      item.genre = genre;
+      item.price = price;
+      item.day = day;
+      item.image = image;
+      return item;
+    }
+    return item;
+  });
+  // console.log(db);
+  res.send({ message: 'OK' }); 
+});
+
+router.route('/concerts/:id').delete((req, res) => {
+  const {id} = req.params;
+  db = db.filter((item) => {
+    return item.id != id
+  });
+  res.send({ message: 'OK' }); 
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,61 +2,20 @@ const express = require('express');
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.json());
 
-let db = [
-  { id: 1, author: 'Jane Doe', text: 'This company is worth every coin!' },
-  { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-];
+
+const testimonialsRoutes = require('./routes/testimonials.routes');
+const seatsRoutes = require('./routes/seats.routes');
+const concertsRoutes = require('./routes/concerts.routes');
+
+app.use('/api', testimonialsRoutes);
+app.use('/api', seatsRoutes);
+app.use('/api', concertsRoutes);
 
 
-app.get('/testimonials', (req, res) => {
-  res.json('db');
-});
 
-app.get('/testimonials/:id', (req, res) => {
-  let newDb = db.filter((item) => { 
-    return item.id == req.params.id;
-  })
-  res.json(newDb);
-});
-
-app.get('/testimonials/random', (req, res) => {
-});
-
-app.post('/testimonials/', (req, res) => {
-  const { author, text } = req.body;
-  db.push({id: (db[db.length -1].id +1), author, text});
-  res.send({ message: 'OK' }); 
-});
-
-app.put('/testimonials/:id', (req, res) => {
-  const { author, text } = req.body;
-  const { id } = req.params;
-
-  db.map((item) => {
-    // console.log(item);
-    if(item.id == id){
-      item.author = author;
-      item.text = text;
-      return item;
-    }
-    return item;
-  });
-  // console.log(db);
-  res.send({ message: 'OK' }); 
-});
-
-app.delete('/testimonials/:id', (req, res) => {
-  const {id} = req.params;
-  db = db.filter((item) => {
-    return item.id != id
-  });
-  res.send({ message: 'OK' }); 
-});
-
-app.use((req, res, next) => {
+app.use((req, res) => {
   res.send({ message: 'Not found' });
 });
 
